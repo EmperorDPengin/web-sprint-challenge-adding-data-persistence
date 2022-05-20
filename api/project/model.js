ac9d0b4fb97dd08894cbe1ec94b3cdd0f4f72b28
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-05-20 09:45:29 -0400

    project model finished

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,27 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig.js');
+
+function transformProjectData(project) {
+    project.project_completed = project.project_completed ? true : false;
+    return project;
+}
+
+function getAll() {
+    return db('projects').then(projects => projects.map( e => transformProjectData(e)));
+}
+
+function getById(id) {
+    return db('projects').where('project_id', id).first();
+}
+
+async function add(project) {
+    const [id] = await db('projects').insert(project);
+
+    return getById(id).then(transformProjectData);
+}
+
+module.exports = {
+    getAll,
+    getById,
+    add
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,11 @@
 // build your `/api/projects` router here
+const express = require('express');
+const Projects = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+
+});
+
+module.exports = router;
