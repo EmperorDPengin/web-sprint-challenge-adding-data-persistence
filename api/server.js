ad9160c3ca482b3e5ef28723913db935191e33b0
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-05-20 09:26:35 -0400

    create projects data schema

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
 // build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+
+//project router
+//resource router
+//task router
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('*', (err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    });
+});
+
+module.exports = server;
